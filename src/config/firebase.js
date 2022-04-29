3b5f928956e5b8a7a605db914430b429dc202ed1
heredia lucas <heredialucasfac22@hotmail.com>
2022-04-29 18:13:09 -0300

    Config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_5RsSbDzVfZoav78HjiAXmeBODURYT9o",
+  authDomain: "utn-web.firebaseapp.com",
+  projectId: "utn-web",
+  storageBucket: "utn-web.appspot.com",
+  messagingSenderId: "680063685107",
+  appId: "1:680063685107:web:9fba5f1999852c46b18286",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
